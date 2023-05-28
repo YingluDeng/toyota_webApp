1cd93db5447d5fbec2fdcc1684f36f80b181f6b2
Cecilia Deng <ceciliadeng12@berkeley.edu>
2023-05-28 02:59:35 -0700

    add text on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
 import CarsCanvas from "./components/canvas/Cars";
-import Container from "./components/Container"
 
 export default function Home() {
   return (
     <div className="w-full h-screen bg-cover bg-center" style={{ backgroundImage: "url(/images/bg_2.jpeg)" }}>
+      <div>
+        <h1 className="absolute font-semi mt-0 text-white lg:text-[75px] sm:text-[60px] xs:text-[40px] text-[40px] lg:leading-[98px] ml-44">
+              Toyota GR Supra
+        </h1>
+      </div>
+        
       <CarsCanvas />
     </div>
   )
